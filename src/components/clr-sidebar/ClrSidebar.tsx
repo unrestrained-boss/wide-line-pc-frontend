4b0ff329bf7622805e9cf397895bc5c40724eb42
shmy <pandahao123>
2019-08-23 13:03:46 +0800

    添加组件测试文件

--- src/components/clr-sidebar/ClrSidebar.tsx
+++ src/components/clr-sidebar/ClrSidebar.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './ClrSidebar.scss'
 import {IMenu} from "../../pages/DashBoardPage";
 import {NavLink} from "react-router-dom";
