2a43f9238ff90dcf5b63a88a2d617cfbf39600c3
shmy <pandahao123>
2019-08-30 10:09:25 +0800

    添加 Profile

--- src/pages/DashBoardPage.tsx
+++ src/pages/DashBoardPage.tsx
@@ -1,11 +1,13 @@
 import React, {Suspense, useState} from 'react';
-import {Layout, Icon, Button} from 'antd';
+import {Layout, Icon, Button, Avatar} from 'antd';
 import WLSidebar from "../components/wl-sidebar/WLSidebar";
 import {Route, Switch} from "react-router";
 import {dashBoardPath} from "../utils/Constant";
 import NotFound from "./NotFound";
 import './DashBoardPage.scss';
 import {menus, routes} from "../utils/Routes";
+import WLModal from "../components/wl-modal/WLModal";
+import UserService from "../services/UserService";
 
 interface Props {
 
@@ -19,25 +21,47 @@ const DashBoardPage: React.FC<Props> = (props) => {
       <Layout.Sider width={240} trigger={null}
                     collapsible
                     collapsed={collapsed}>
-        <div className={"header"}>
-          <img className="header-logo" src="http://cdn.admui.com/demo/iframe/2.1.0/images/logo-white-min.svg" alt=""/>
-          <div style={{display: collapsed ? 'none' : 'inline'}} className="header-title">
+        <div className={"sider-header"}>
+          <img className="sider-logo" src="http://cdn.admui.com/demo/iframe/2.1.0/images/logo-white-min.svg" alt=""/>
+          <div style={{display: collapsed ? 'none' : 'inline'}} className="sider-title">
             <span>国土战略防御局</span>
-            <span className={"sub-title"}>S.H.I.E.L.D</span>
+            <span className={"sider-sub-title"}>S.H.I.E.L.D</span>
           </div>
         </div>
         <WLSidebar collapsed={collapsed} menus={menus}/>
       </Layout.Sider>
       <Layout>
-        <Layout.Header>
-          <Button onClick={() => {
+        <Layout.Header className={"header"}>
+
+          <Icon onClick={() => {
             setCollapsed(!collapsed);
-          }}>
-            <Icon
-              className="trigger"
-              type={collapsed ? 'menu-unfold' : 'menu-fold'}
-            />
-          </Button>
+          }}
+                className={"header-trigger"}
+                type={collapsed ? 'menu-unfold' : 'menu-fold'}
+          />
+          <div className="header-expand"/>
+          <div className="header-actions">
+            <div className="profile-wrap">
+              <Avatar style={{color: '#f56a00', backgroundColor: '#fde3cf'}}>
+                面
+              </Avatar>
+              <span className="profile-username">面对疾风吧</span>
+              <ul className="profile-popup-wrap">
+                <li className="profile-item">账户信息</li>
+                <li className="profile-item">修改密码</li>
+                <li onClick={() => {
+                  WLModal.confirm('确认退出吗?', {
+                    onOk: ({close}) => {
+                      UserService.logoutUser();
+                      window.location.href = process.env.REACT_APP_BASE_URL as string;
+                      close();
+                    }
+                  });
+                }} className="profile-item">退出登录
+                </li>
+              </ul>
+            </div>
+          </div>
         </Layout.Header>
         <Layout.Content className={"router-outlet"}>
           <Suspense fallback={null}>
